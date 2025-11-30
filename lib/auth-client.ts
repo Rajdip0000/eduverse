b705cd69b740d222c079f623ea35dae110f5ef59
Rajdip0000 <opo44264@gmail.com>
2025-11-30 11:26:24 +0530

    fix: create useSession with authClient.\ store

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,16 @@
 // @ts-ignore - better-auth has type export issues
 import { createAuthClient } from 'better-auth/client'
+// @ts-ignore
+import { useStore } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.BETTER_AUTH_URL || 'http://localhost:3000'
 })
 
-export const { signIn, signUp, signOut, useSession } = authClient
+export const { signIn, signUp, signOut } = authClient
+
+// Create useSession hook using Better Auth's useStore
+export function useSession() {
+  // @ts-ignore
+  return useStore(authClient.$session)
+}
