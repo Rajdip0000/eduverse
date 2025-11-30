829109afc447ffc0a48c2c0c3bd3b60a9a5c432e
Rajdip0000 <opo44264@gmail.com>
2025-11-30 11:29:36 +0530

    fix: implement custom useSession hook with fetch

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
+'use client'
+
 // @ts-ignore - better-auth has type export issues
 import { createAuthClient } from 'better-auth/client'
-// @ts-ignore
-import { useStore } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.BETTER_AUTH_URL || 'http://localhost:3000'
@@ -9,8 +9,25 @@ export const authClient = createAuthClient({
 
 export const { signIn, signUp, signOut } = authClient
 
-// Create useSession hook using Better Auth's useStore
+// Simple session hook that fetches session on mount
 export function useSession() {
-  // @ts-ignore
-  return useStore(authClient.$session)
+  const [session, setSession] = React.useState<any>(null)
+  const [isPending, setIsPending] = React.useState(true)
+  const [error, setError] = React.useState<any>(null)
+
+  React.useEffect(() => {
+    authClient.$fetch('/session/get')
+      .then((res: any) => {
+        setSession(res.data)
+        setIsPending(false)
+      })
+      .catch((err: any) => {
+        setError(err)
+        setIsPending(false)
+      })
+  }, [])
+
+  return { data: session, isPending, error }
 }
+
+import * as React from 'react'
