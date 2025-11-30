1f99f6c5c5894b410682a1d92c59d93b75bc6a81
Rajdip0000 <opo44264@gmail.com>
2025-11-30 14:31:07 +0530

    fix: use Better Auth's built-in useSession hook for proper OAuth session handling

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,5 @@
 'use client'
 
-import * as React from 'react'
 // @ts-ignore - better-auth has type export issues
 import { createAuthClient } from 'better-auth/client'
 
@@ -8,25 +7,4 @@ export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'http://localhost:3000'
 })
 
-export const { signIn, signUp, signOut } = authClient
-
-// Simple session hook that fetches session on mount
-export function useSession() {
-  const [session, setSession] = React.useState<any>(null)
-  const [isPending, setIsPending] = React.useState(true)
-  const [error, setError] = React.useState<any>(null)
-
-  React.useEffect(() => {
-    authClient.$fetch('/session/get')
-      .then((res: any) => {
-        setSession(res.data)
-        setIsPending(false)
-      })
-      .catch((err: any) => {
-        setError(err)
-        setIsPending(false)
-      })
-  }, [])
-
-  return { data: session, isPending, error }
-}
+export const { signIn, signUp, signOut, useSession } = authClient
