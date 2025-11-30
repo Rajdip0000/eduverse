b121ac8df3b7494d30041d455ccacc07f2196513
Rajdip0000 <opo44264@gmail.com>
2025-11-30 11:18:55 +0530

    fix: import Suspense in reset-password page

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Image from 'next/image'
 import logo from '../images/logo.jpg'
