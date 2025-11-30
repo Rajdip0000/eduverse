1fa5aa43f351b6924a8bea6fc547f68fb5b2a5cb
Rajdip0000 <opo44264@gmail.com>
2025-11-30 11:14:59 +0530

    fix: add ts-ignore for better-auth type issues

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,9 @@
-import * as betterAuthClient from 'better-auth/client'
+// @ts-ignore - better-auth has type export issues
+import { createAuthClient } from 'better-auth/client'
+// @ts-ignore
 import { useStore } from 'better-auth/react'
 
-export const authClient = betterAuthClient.createAuthClient({
+export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.BETTER_AUTH_URL || 'http://localhost:3000'
 })
 
